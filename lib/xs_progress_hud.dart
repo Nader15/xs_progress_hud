17502e1859779ad63c8bce4fbf9e3eb3f832b59b
Nader Salah <47080949+Nader15@users.noreply.github.com>
2021-08-30 21:02:52 +0200

    Update xs_progress_hud.dart

--- lib/xs_progress_hud.dart
+++ lib/xs_progress_hud.dart
@@ -124,7 +124,7 @@ class XsProgressHud extends PopupRoute {
             children: <Widget>[
              Center(
                         child: Image.asset(
-                      "assets/images/loading_logo.gif",
+                      "assets/images/animated_logo3.gif",
                       width: 300,
                     )),
 //                Center(
